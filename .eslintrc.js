f4811f1fbfac767a919cb5438649713010b9460c
hobutt <hobutt.ne@bk.ru>
2022-09-14 20:40:46 +0300

    Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: "airbnb-base",
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+		"no-console": 0,
+		quotes: ["error", "double"],
+		indent: 0,
+		"no-tabs": 0,
+  },
+};
